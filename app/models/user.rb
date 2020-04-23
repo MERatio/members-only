152eda404f4d0df3fa1cf30271c7f99b091830d2
Mark Edward Ratio <markedwardratio@gmail.com>
2020-04-23 12:20:46 +0800

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
   before_save :downcase_email
   before_create :set_remember_token
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { in: 6..16 }
 
   # Returns a random 22 length token
   def User.new_token
